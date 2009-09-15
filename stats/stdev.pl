d7173254e72c4af416a32f96fb7dbaebc9ff0527
dacaplan <dacaplan@tera.sickkids.ca>
2009-09-15 11:18:06 -0400

    added some stuff to stdev

--- stats/stdev.pl
+++ stats/stdev.pl
@@ -7,6 +7,8 @@ my $N = 0;
 my @X;
 my $SS = 0;
 my $avg;
+my $min = 999999;
+my $max = -1;
 while( my $line = <stdin> ){
      chomp $line;
      next if ($line =~ m/^[ \#]/);
@@ -15,11 +17,22 @@ while( my $line = <stdin> ){
      push(@X, $line);
      $N ++;
      $sum += $line;
+     if ($line > $max) {
+         $max = $line;
+     }
+     if ($line < $min) {
+         $min = $line;
+     }
 }
 $avg = $sum/$N;
+print "Mean: $avg\n";
+print "Min: $min\n";
+print "Max: $max\n";
+
 foreach my $X (@X){
      $SS += ($X - $avg)**2;
 }
 my $stdev = sqrt($SS/($N-1));
+print "Stdev: ";
 print $stdev/sqrt($N);
 print "\n";
