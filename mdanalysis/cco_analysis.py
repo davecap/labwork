ea19d02f9567609f17272aabc2a1113b58d5ce5a
David Caplan <dcaplan@gmail.com>
2010-08-17 00:49:41 -0400

    modify cco analysis

--- mdanalysis/cco_analysis.py
+++ mdanalysis/cco_analysis.py
@@ -5,82 +5,88 @@ import optparse
 import numpy
 import numpy.linalg
 import scipy.stats
-from MDAnalysis import *
-from MDAnalysis import collection, SelectionError
-import MDAnalysis.core.rms_fitting
-from MDAnalysis.core.AtomGroup import Residue, AtomGroup
 from math import pi
 
+from tables import *
+from MDAnalysis import *
 from analysis import *
 from rmsd import RMSD
 
-from tables import *
-
-
-# Datastore Attributes
 #   TODO: parse NAMD config file on each analysis
-
-#   dt
-#   dcdtime
-#   pdb
-#   psf
 
 def main():
     usage = """
         usage: %prog [options] <PSF file> <PDB file> <DCD file>
     """
+    
+    # initialize the parser with our custom usage string (above)
     parser = optparse.OptionParser(usage)
+    # add all the command-line options
     parser.add_option("-o", dest="h5_filename", default="analysis.h5", help="Output analysis file [default: %default]")    
-    
     parser.add_option("-s", dest="selection", default="name CA", help="Atom selection [default: %default]")    
-    parser.add_option("-t", dest="first_timestep", default="0", help="Starting timestep [default: %default]")    
-    parser.add_option("-f", dest="dcdtime", default="1", help="DCD output frequency [default: %default]")    
-    parser.add_option("-d", dest="dt", default="0.002", help="Integration Timestep [default: %default]")    
+    parser.add_option("-t", dest="first_timestep", default=0, type="int", help="Starting timestep [default: %default]")    
+    parser.add_option("-f", dest="dcdtime", default=1, type="int", help="DCD output frequency [default: %default]")    
+    parser.add_option("-d", dest="dt", default=0.002, type="float", help="Integration Timestep [default: %default]")    
     
-    options, args = parser.parse_args()
+    # parse the command line
+    (options, args) = parser.parse_args()
     
+    # check to make sure all arguments are there
     if len(args) < 3:
         parser.error("No input files specified")
     
     psf_file = args[0]
     pdb_file = args[1]
     dcd_file = args[2]
-    dcdtime = int(options.dcdtime)
-    dt = float(options.dt)
-    first_timestep = int(options.first_timestep)
     
-    if not psf_file.lower().endswith('.psf'):
+    # do some error checking for required file paths
+    if not os.path.exists(psf_file):
+        print "Error: PSF file doesn't exist: %s" % psf_file
+        return
+    elif not psf_file.lower().endswith('.psf'):
         print "Warning: PSF filename does not end in .psf: %s" % psf_file
-    if not pdb_file.lower().endswith('.pdb'):
-        print "Warning: PDB filename does not end in .pdb: %s" % pdb_file
-    if not dcd_file.lower().endswith('.dcd'):
-        print "Warning: DCD filename does not end in .dcd: %s" % dcd_file
     
+    if not os.path.exists(pdb_file):
+        print "Error: PDB file doesn't exist: %s" % pdb_file
+        return
+    elif not pdb_file.lower().endswith('.pdb'):
+        print "Warning: PDB filename does not end in .pdb: %s" % pdb_file
+    
+    if not os.path.exists(dcd_file):
+        print "Error: DCD file doesn't exist: %s" % dcd_file
+        return
+    elif not dcd_file.lower().endswith('.dcd'):
+        print "Warning: DCD filename does not end in .dcd: %s" % dcd_file    
+
     # check to make sure we haven't analyzed this DCD yet
-    # TODO: run md5 in subprocess...?
     if os.path.exists(options.h5_filename):
+        # open the HDF5 database
         h5f = tables.openFile(options.h5_filename, mode="r")
+        # get the metadata table
         tbl = h5f.getNode('/metadata/trajectory')
+        # get the list of analyzed dcd files
         analyzed_dcd_files = tbl.read(field='dcd')
         h5f.close()
         if dcd_file.split('/')[-1] in analyzed_dcd_files:
             print "DCD file %s already analyzed... exiting!" % dcd_file
             return -1
     
+    # load the PSF and PDB into the analysis system
     print "Loading reference system: %s, %s" % (psf_file, pdb_file)
     ref = Universe(psf_file, pdb_file)
-    
     print "Loading trajectory: %s" % (dcd_file)
     trj = Universe(psf_file, dcd_file)
     
+    # calculate frames and timesteps
     num_frames = trj.trajectory.numframes
     # frame i is first_timestep + dcdtime*i for timestep
     # frame i is (first_timestep + dcdtime*i)*dt for time
-    frame_range = numpy.array(range(1,num_frames+1))*dcdtime+first_timestep
-    time_range = frame_range*dt
+    frame_range = numpy.array(range(1,num_frames+1))*options.dcdtime+options.first_timestep
+    time_range = frame_range*options.dt
     
+    # add the metadata for this analysis to the database
     analysis = Analysis(options.h5_filename, readonly=False)
-    analysis.add_metadata('/metadata/trajectory', { 'psf': psf_file.split('/')[-1], 'pdb': pdb_file.split('/')[-1], 'dcd': dcd_file.split('/')[-1], 'frames': num_frames, 'firsttimestep': first_timestep, 'dt': dt })
+    analysis.add_metadata('/metadata/trajectory', { 'psf': psf_file.split('/')[-1], 'pdb': pdb_file.split('/')[-1], 'dcd': dcd_file.split('/')[-1], 'frames': num_frames, 'firsttimestep': options.first_timestep, 'dt': options.dt })
     
     # RMSD
     # this takes too long so I'm disabling it for now
