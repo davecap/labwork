14bc617fe020226dd0278a1636ddec9e1a34c688
David Caplan <dcaplan@gmail.com>
2011-03-05 12:52:40 -0500

    fix up replica creation script

--- workflows/kbinding/replica_creation/generate.py
+++ workflows/kbinding/replica_creation/generate.py
@@ -22,24 +22,33 @@ def main():
     template_ensemble = umbrellas.Ensemble(template_config)
     output_ensemble = umbrellas.Ensemble(output_config)
     
-    output_path = os.path.dirname(output_config)
-    output_pdb_path = os.path.dirname(output_config)+'/pdbs'
-    os.mkdir(output_pdb_path)
+    template_path = template_ensemble.basedir()
+    output_path = output_ensemble.basedir()
+    output_pdb_path = os.path.join(output_path,'pdbs')
+
+    if not os.path.exists(output_pdb_path):
+        sys.stderr.write("Creating output PDB path: %s\n" % output_pdb_path)
+        os.mkdir(output_pdb_path)
         
     # for each template replica
     #   align a with starting structure
     #   create new output replica and PDB
     for i,t in enumerate(template_ensemble.get_replicas()):
-        template_pdb = t.get_parameter('coordinates')
-        template_psf = template_ensemble.config['psf']
-        output_pdb = output_pdb_path+'/%d.pdb'%i
+        sys.stderr.write("Aligning template %d\n" % i)
+        template_pdb = os.path.join(template_path,t.parameter('coordinates'))
+        template_psf = os.path.join(output_path, template_ensemble.config['psf'])
+        output_pdb = os.path.join(output_pdb_path,'%d.pdb'%i)
         
+        # Align using the VMD TCL script
         tcl = stage.pdb_stage_tcl(staging_pdb=template_pdb, starting_pdb=starting_pdb, align_selection="protein and backbone", target_selection="protein and backbone", output_pdb=output_pdb)
-        #run_VMD(tcl, options.verbose)
-        print tcl
+        stage.run_VMD(tcl)
         
-        #r = output_ensemble.add_replica(name='%d'%i, **t._parameters)
-        #r._parameters['coordinate'] = output_pdb
+        # Create the new replica with the same parameters but set the PDB path
+        r = output_ensemble.add_replica(name='%d'%i, **t.parameters)
+        r.parameters['coordinates'] = 'pdbs/%d.pdb'%i
+        output_ensemble.save()
+
+    sys.stderr.write("Created %d replicas from starting structure %s" % (i+1, starting_pdb))
                     
 if __name__ == "__main__":
     main()
--- workflows/kbinding/replica_creation/stage.py
+++ workflows/kbinding/replica_creation/stage.py
@@ -30,7 +30,7 @@ def main():
     """
     
     parser = optparse.OptionParser(usage)
-    parser.add_option("-o", "--output-prefix", dest="output_prefix", default="aligned", help="Output file prefix [default: %default]")
+    parser.add_option("-o", "--output-pdb", dest="output_pdb", default="aligned", help="Output PDB [default: %default]")
     parser.add_option("-a", "--align-selection", dest="align_selection", default="protein and backbone", help="VMD selection to align by [default: %default]")
     parser.add_option("-t", "--target-selection", dest="target_selection", default="resid 1", help="VMD target selection to stage [default: %default]")
     parser.add_option("-p", "--psf", dest="psf_file", default=None, help="PSF structure file for DCDs [default: %default]")
@@ -48,21 +48,21 @@ def main():
     if args[0].lower().endswith('.dcd'):
         if options.psf_file is None:
             parser.error("PSF file required (-p) if using a DCD trajectory file")
-        tcl = dcd_stage_tcl(staging_psf=options.psf_file, staging_dcd=args[0], starting_pdb=args[1], align_selection=options.align_selection, target_selection=options.target_selection, output_prefix=options.output_prefix)   
+        tcl = dcd_stage_tcl(staging_psf=options.psf_file, staging_dcd=args[0], starting_pdb=args[1], align_selection=options.align_selection, target_selection=options.target_selection, output_pdb=options.output_pdb)   
     elif args[0].lower().endswith('.pdb'):
-        tcl = pdb_stage_tcl(args[0], starting_pdb=args[1], align_selection=options.align_selection, target_selection=options.target_selection, output_prefix=options.output_prefix)
+        tcl = pdb_stage_tcl(args[0], starting_pdb=args[1], align_selection=options.align_selection, target_selection=options.target_selection, output_pdb=options.output_pdb)
     else:
         parser.error("Invalid input file, must be a PDB or DCD file")
     
     run_VMD(tcl, options.verbose)
 
-def dcd_stage_tcl(staging_psf, staging_dcd, starting_pdb, align_selection, target_selection, output_prefix="staged"):
+def dcd_stage_tcl(staging_psf, staging_dcd, starting_pdb, align_selection, target_selection, output_pdb="staged.pdb"):
     data = {    'staging_psf': staging_psf,
                 'staging_dcd': staging_dcd,
                 'starting_pdb': starting_pdb,
                 'align_selection': align_selection,
                 'target_selection': target_selection,
-                'output_prefix': output_prefix,
+                'output_pdb': output_pdb,
             }
 
     tcl_template = """mol load psf "$staging_psf" dcd "$staging_dcd"
@@ -88,18 +88,18 @@ for {set frame 0} {$$frame < $$num_steps} {incr frame} {
     # move target in starting (1) to position of the same target in staging (0)
     set trans_mat [measure fit $$sel1target $$sel0target]
     $$sel1target move $$trans_mat
-    $$sel1all writepdb "${output_prefix}_$$frame.pdb"
+    $$sel1all writepdb "${output_pdb}_$$frame.pdb"
 }
 quit
 """
     return Template(tcl_template).substitute(data)
  
-def pdb_stage_tcl(staging_pdb, starting_pdb, align_selection, target_selection, output_prefix="staged"):    
+def pdb_stage_tcl(staging_pdb, starting_pdb, align_selection, target_selection, output_pdb="staged"):    
     data = {    'staging_pdb': staging_pdb,
                 'starting_pdb': starting_pdb,
                 'align_selection': align_selection,
                 'target_selection': target_selection,
-                'output_prefix': output_prefix,
+                'output_pdb': output_pdb,
             }
     
     tcl_template = """mol load pdb "$staging_pdb"
@@ -121,7 +121,7 @@ set sel1target [atomselect 1 "$target_selection"]
 set trans_mat [measure fit $$sel1target $$sel0target]
 $$sel1target move $$trans_mat
 
-$$sel1all writepdb "$output_prefix.pdb"
+$$sel1all writepdb "$output_pdb"
 quit
 """ 
     return Template(tcl_template).substitute(data)    
